64d2337ef5f30fa18069a415f0f63ac9b5b2cbf4
Jennifer Lin <jennifer.lin16@ncf.edu>
2019-02-17 21:47:35 -0500

    Modified

---
+++ CSES4Analysis/Management.R
@@ -0,0 +1,7 @@
+#CSES Module 4 Data Cleaning 
+#Author: Jennifer Lin
+#Purpose: Clean Data - Also, generate an R version of Stata analysis
+
+#Read in Data
+load("/Users/JenniferLin/Desktop/Working/CSES/cses4.rdata")
+
