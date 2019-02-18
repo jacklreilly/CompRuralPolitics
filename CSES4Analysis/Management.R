ac557a9095723787194bb761d0694fa8cf90f4b2
Jennifer Lin <jennifer.lin16@ncf.edu>
2019-02-17 21:48:40 -0500

    Began cleaning CSES 4

--- CSES4Analysis/Management.R
+++ CSES4Analysis/Management.R
@@ -3,5 +3,13 @@
 #Purpose: Clean Data - Also, generate an R version of Stata analysis
 
 #Read in Data
-load("/Users/JenniferLin/Desktop/Working/CSES/cses4.rdata")
+data <- read.csv("/Users/JenniferLin/Desktop/Working/CSES/cses4.csv")
+
+library(car) #statistics
+library(dplyr) #Recoding
+library(gmodels) #Crosstabulations
+
+#Recode data using the car package
+data$rural <- recode(data$D2031, "7:9 = '.'")
+CrossTable(data$D2031, data$rural)
 
